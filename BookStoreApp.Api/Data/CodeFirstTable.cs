52904b537a57f54dad8413ad0552db0b8d2ac6fe
mcct <mcct@privat.dk>
2024-01-30 20:53:21 +0100

    Added EntityFrameworkCore And Scaffolded Database

---
+++ BookStoreApp.Api/Data/CodeFirstTable.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStoreApp.Api.Data
+{
+
+    // This is not actually used in the final app !
+
+    // Showing how to do a codefirst aproch instead of scafolding from an existing database video 16 - min 13.44
+    public class CodeFirstTable
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        // So after adding the properties. i need to manually add them to the BookStoreDbContext class
+        // And do migration in nuget packet manager
+    }
+}
